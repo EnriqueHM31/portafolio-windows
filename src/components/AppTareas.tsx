d1fa8c143cc58676285dc74dfc1bf90c317e0eb7
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-08-09 21:27:53 -0600

    Refactor AppTareas component to improve active state styling with a new underline effect and adjust button structure for better layout.

--- src/components/AppTareas.tsx
+++ src/components/AppTareas.tsx
@@ -5,10 +5,16 @@ interface AppTareasProps {
 
 export default function AppTareas({ icono, active }: AppTareasProps) {
     return (
-        <button className={`flex items-center gap-4 h-full group ${active ? 'border-blue-300' : 'border-transparent'} border-b-2 p-1 hover:bg-white/20 transition duration-200 ease-in cursor-pointer`}>
+        <button className={`flex items-center gap-4 h-full group  p-1 hover:bg-white/20 transition duration-200 ease-in cursor-pointer relative`}>
+            {
+                active && (
+                    <div className="absolute left-0  bottom-0  w-full h-[2px] bg-blue-300 rounded scale-x-75 group-hover:scale-x-100 transition duration-300 ease-in-out"></div>
+                )
+            }
+
             <div className={`w-full h-full p-1 flex items-center justify-center`}>
                 <img src={icono} alt="icono" className="max-w-10/12" />
             </div>
-        </button>
+        </button >
     )
 }
