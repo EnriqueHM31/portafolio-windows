9e4bcdb48723f88c9b6abc9fc6a7c4f733b41ece
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-08-10 16:02:48 -0600

    Add Wifi component for internet access display with tooltip and modal integration, enhancing user interaction in BarraTareas.

---
+++ src/components/barraTareas/Wifi.tsx
@@ -0,0 +1,27 @@
+import { FaWifi } from "react-icons/fa";
+import Tooltip from "@/components/general/ToolTip";
+import { AnimatePresence } from "framer-motion";
+import Container from "../secciones/Container";
+import { useOpen } from "@/hooks/general/useOpen";
+import WifiModal from "@/components/secciones/Wifis";
+
+export default function Wifi() {
+
+    const { isOpen, handleOpen, handleClose } = useOpen()
+    return (
+        <Tooltip text="INFINITUM_LEHM<br/>Acceso a Internet" position="top">
+            <span className='hover:bg-white/20 h-full flex items-center justify-center px-1 cursor-pointer' onClick={() => handleOpen("Wifi-Tareas")}>
+                <AnimatePresence>
+                    {
+                        isOpen && (
+                            <Container className="w-102 h-115 -left-40 -top-115 overflow-y-auto scrollbar-none" onClose={handleClose}>
+                                <WifiModal />
+                            </Container>
+                        )
+                    }
+                </AnimatePresence>
+                <FaWifi className='text-xl' />
+            </span>
+        </Tooltip>
+    )
+}
