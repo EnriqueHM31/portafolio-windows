91361355895225f855926f079bda9cbb55f4f8a9
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-08-10 10:40:19 -0600

    Add BarraTareas component for taskbar functionality, including app shortcuts, search input, and real-time clock display.

---
+++ src/components/BarraTareas.tsx
@@ -0,0 +1,92 @@
+import { FaWindows } from "react-icons/fa";
+import Tooltip from "./general/ToolTip";
+import { VscSearch } from "react-icons/vsc";
+import { FaWifi } from "react-icons/fa";
+import { LiaComment } from "react-icons/lia";
+import { PiBatteryHighFill } from "react-icons/pi";
+import { IoVolumeHighSharp } from "react-icons/io5";
+import { APPS_PREDETERMINADAS } from "../constants/Aplicaciones";
+import AppTareas from "./AppTareas";
+import { useTiempo } from "@/hooks/barraTareas/UseTiempo";
+
+export default function BarraTareas() {
+
+    const { hora, fecha, fechaFinal } = useTiempo()
+    return (
+        <section className="relative w-full z-10 bg-black/90 text-white flex items-center max-w-full mx-auto ">
+            {/* Botón de inicio */}
+            <div className='flex items-center h-full group '>
+                <Tooltip text="Inicio" position="top">
+                    <button className=" size-12 flex items-center justify-center">
+                        <FaWindows className='text-xl' />
+                    </button>
+                </Tooltip>
+                <label
+                    htmlFor="buscador-windows"
+                    className="flex items-center gap-2 bg-primary h-full ps-3 pe-4 border-2 border-transparent focus-within:border-blue-500 "
+                >
+                    <VscSearch className="text-secondary" />
+                    <input
+                        type="search"
+                        id="buscador-windows"
+                        className="text-secondary appearance-none outline-none w-58"
+                        placeholder="Buscar"
+                    />
+                </label>
+            </div>
+
+            {/* Espacio en medio */}
+            <div className="flex-1 flex items-center gap-1 px-1">
+                {
+                    APPS_PREDETERMINADAS.map((app) => (
+                        <Tooltip text={app.label} key={app.id} position="top">
+                            <AppTareas key={app.id} icono={app.icono} active={app.active} />
+                        </Tooltip>
+                    ))
+                }
+            </div>
+
+            {/* Hora */}
+            <div className='flex items-center gap-2 ps-4 h-full'>
+                <Tooltip text="INFINITUM_LEHM<br/>Acceso a Internet" position="top">
+                    <span className='hover:bg-white/20 h-full flex items-center justify-center px-1 cursor-pointer'>
+                        <FaWifi className='text-xl' />
+                    </span>
+                </Tooltip>
+
+                <Tooltip text="80 % Disponible" position="top">
+                    <span className='hover:bg-white/20 h-full flex items-center justify-center px-1 cursor-pointer'>
+                        <PiBatteryHighFill className='text-xl' />
+
+                    </span>
+                </Tooltip>
+
+                <Tooltip text="Altavoces: 100%" position="top">
+                    <span className='hover:bg-white/20 h-full flex items-center justify-center px-1 cursor-pointer'>
+                        <IoVolumeHighSharp className=' text-xl ' />
+                    </span>
+                </Tooltip>
+
+                <Tooltip text={fechaFinal} position="top">
+                    <div className='flex flex-col hover:bg-white/20 h-full justify-center px-1 cursor-pointer'>
+                        <span className='text-xs'>{hora}</span>
+                        <span className='text-xs'>{fecha}</span>
+
+                    </div>
+                </Tooltip>
+
+                <Tooltip text="Notificaciones no disponibles" position="top_left">
+                    <span className='hover:bg-white/20 h-full flex items-center justify-center px-2 cursor-pointer'>
+
+                        <LiaComment className=' text-2xl' />
+                    </span>
+                </Tooltip>
+
+
+                <div className='border-s border-white/50 h-11 w-2 hover:bg-white/20 cursor-pointer'>
+
+                </div>
+            </div>
+        </section>
+    )
+}
