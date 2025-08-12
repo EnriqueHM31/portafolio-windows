f6b4dee02a64fa387a396257529a63f7daef669b
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-08-12 17:41:53 -0600

    Update Wifi component to reflect connection status with dynamic icons
    - Integrate Zustand store to manage Wi-Fi states, allowing the component to check for connected networks.
    - Replace static Wi-Fi icon with dynamic icons based on connection status, enhancing user feedback.
    - Improve code structure by removing unused imports and organizing component logic.

--- src/components/barraTareas/Wifi.tsx
+++ src/components/barraTareas/Wifi.tsx
@@ -1,12 +1,17 @@
-import { FaWifi } from "react-icons/fa";
 import Tooltip from "@/components/general/ToolTip";
-import { AnimatePresence } from "framer-motion";
-import Container from "../secciones/Container";
-import { useOpen } from "@/hooks/general/useOpen";
 import WifiModal from "@/components/secciones/Wifis";
+import { useOpen } from "@/hooks/general/useOpen";
+import { useStoreWifis } from "@/store/barraTareas/Wifis";
+import { AnimatePresence } from "framer-motion";
+import { FiWifiOff } from "react-icons/fi";
+import { IoWifiOutline } from "react-icons/io5";
+import Container from "../secciones/Container";
+
 
 export default function Wifi() {
 
+    const { wifis } = useStoreWifis();
+    const HayUnWifiConectado = wifis.find(w => w.conectado);
     const { isOpen, handleOpen, handleClose } = useOpen()
     return (
         <Tooltip text="INFINITUM_LEHM<br/>Acceso a Internet" position="top">
@@ -20,7 +25,13 @@ export default function Wifi() {
                         )
                     }
                 </AnimatePresence>
-                <FaWifi className='text-xl' />
+                {
+                    HayUnWifiConectado ? (
+                        <IoWifiOutline className='text-xl' />
+                    ) : (
+                        <FiWifiOff className='text-xl' />
+                    )
+                }
             </span>
         </Tooltip>
     )
