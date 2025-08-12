27d7a3eb3ae457ea5e247da13d4097f55ce33580
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-08-11 20:13:55 -0600

    Enhance AjustesPredeterminadosIcon component with dynamic color support
    - Introduce COLORES_AJUSTESPREDETERMINADOS for icon color management based on active state.
    - Update component props to include 'active' for conditional styling.
    - Refactor rendering logic to apply dynamic classes for improved visual feedback.

--- src/components/Iconos/AjustesPredeterminados.tsx
+++ src/components/Iconos/AjustesPredeterminados.tsx
@@ -12,8 +12,17 @@ const ICONOS_AJUSTESPREDETERMINADOS: Record<string, IconType> = {
     MdBrightness4
 };
 
+const COLORES_AJUSTESPREDETERMINADOS: Record<string, string> = {
+    SiGooglemaps: "text-red-300",
+    MdEco: "text-green-500",
+    FaBluetoothB: "text-blue-400",
+    MdBrightness4: "text-yellow-300"
+};
+
 // Ejemplo de uso con valor dinámico
-export default function AjustesPredeterminadosIcon({ nombreIcono, styles }: { nombreIcono: string, styles?: string }) {
+export default function AjustesPredeterminadosIcon({ nombreIcono, styles, active }: { nombreIcono: string, styles?: string, active?: boolean }) {
     const Icono = ICONOS_AJUSTESPREDETERMINADOS[nombreIcono];
-    return Icono ? <Icono className={styles} /> : null;
+    const clasesIcono = active ? COLORES_AJUSTESPREDETERMINADOS[nombreIcono] : "";
+
+    return Icono ? <Icono className={` ${styles} ${clasesIcono}`} /> : null;
 }
