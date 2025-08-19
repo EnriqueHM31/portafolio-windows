48ac32d07e775c0a94748f245ec7b27842df810a
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-08-19 10:03:27 -0600

    Enhance AppTareas component with animation and conditional rendering
    - Integrate framer-motion for improved animation effects on the AppTareas button.
    - Add 'barra' prop to conditionally render the button, enhancing user experience.
    - Refactor rendering logic to maintain clarity and structure while incorporating the new animation features.

--- src/components/aplicaciones/AppTareas.tsx
+++ src/components/aplicaciones/AppTareas.tsx
@@ -1,24 +1,33 @@
-import AplicacionesPredeterminadasIcon from "../Iconos/AplicacionesPredeterminadas"
+import { motion, AnimatePresence } from "framer-motion";
+import AplicacionesPredeterminadasIcon from "../Iconos/AplicacionesPredeterminadas";
 
 interface AppTareasProps {
-    icono: string
-    active: boolean
+    barra: boolean;
+    icono: string;
+    active: boolean;
 }
 
-export default function AppTareas({ icono, active }: AppTareasProps) {
+export default function AppTareas({ barra, icono, active }: AppTareasProps) {
     return (
-        <button className={`flex items-center gap-4 h-full group  p-1 hover:bg-white/20 transition duration-200 ease-in cursor-pointer relative`}>
-            {
-                active && (
-                    <div className="absolute left-0  bottom-0  w-full h-[2px] bg-blue-300 rounded scale-x-75 group-hover:scale-x-100 transition duration-300 ease-in-out"></div>
-                )
-            }
+        <AnimatePresence mode="wait">
+            {barra && ( // 👈 importante: solo se renderiza si barra = true
+                <motion.button
+                    key={icono}
+                    initial={{ y: 30, scale: 0, opacity: 0 }}
+                    animate={{ y: 0, scale: 1, opacity: 1 }}
+                    exit={{ y: -30, scale: 0, opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                    className="flex items-center gap-4 h-full group p-1 hover:bg-white/20 transition duration-200 ease-in cursor-pointer relative"
+                >
+                    {active && (
+                        <div className="absolute left-0 bottom-0 w-full h-[2px] bg-blue-300 rounded scale-x-75 group-hover:scale-x-100 transition duration-300 ease-in-out"></div>
+                    )}
 
-            <div className={`w-full h-full p-1 flex items-center justify-center`}>
-                {
-                    AplicacionesPredeterminadasIcon({ nombreIcono: icono })
-                }
-            </div>
-        </button >
-    )
+                    <div className="w-full h-full p-1 flex items-center justify-center">
+                        {AplicacionesPredeterminadasIcon({ nombreIcono: icono })}
+                    </div>
+                </motion.button>
+            )}
+        </AnimatePresence>
+    );
 }
