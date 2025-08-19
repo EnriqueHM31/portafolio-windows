6dfefa1e01b25dab0a79baded891a92d4fb4fbf6
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-08-19 10:03:51 -0600

    Refactor Escritorio component to utilize updated application state management
    - Rename variable from 'aplicacionesPredeterminadas' to 'aplicacionesEscritorio' for clarity.
    - Introduce Aplicacion interface to standardize application properties.
    - Update rendering logic to accommodate new application structure and improve maintainability.

--- src/components/secciones/aplicaciones/Escritorio.tsx
+++ src/components/secciones/aplicaciones/Escritorio.tsx
@@ -1,12 +1,18 @@
 import AplicacionIcono from "@/components/aplicaciones/AplicacionIcono";
-import { GooglePage } from "@/components/pages";
 import { useStoreAplicacionesPredeterminadas } from "@/store/aplicaciones/AplicacionesPredeterminadas";
 import { useState } from "react";
 
+interface Aplicacion {
+    id: number;
+    label: string;
+    icono: string;
+    escritorio: boolean;
+    barraTareas: boolean;
+}
 
 export default function Escritorio() {
 
-    const { aplicacionesPredeterminadas, onRemove, onRename } = useStoreAplicacionesPredeterminadas();
+    const { aplicacionesEscritorio, onRemove, onRename } = useStoreAplicacionesPredeterminadas();
     const [submenuAbiertoId, setSubmenuAbiertoId] = useState<number | null>(null);
 
     // Cierra submenu si clickeas fuera
@@ -20,13 +26,13 @@ export default function Escritorio() {
             onClick={handleClickFuera}
             style={{ minHeight: "90vh" }}
         >
-            <GooglePage />
-            {aplicacionesPredeterminadas.map(({ id, icono, label }) => (
+            {
+
+            }
+            {aplicacionesEscritorio.map((aplicacion: Aplicacion) => (
                 <AplicacionIcono
-                    key={id}
-                    id={id}
-                    icono={icono}
-                    name={label}
+                    key={aplicacion.id}
+                    aplicacion={aplicacion}
                     submenuAbiertoId={submenuAbiertoId}
                     setSubmenuAbiertoId={setSubmenuAbiertoId}
                     onRemove={onRemove}
